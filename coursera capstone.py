71c08ca61c6830d89b8ae463effc88986293e784
Timo2413 <70541048+Timo2413@users.noreply.github.com>
2020-09-17 15:27:41 -0400

    Add files via upload

---
+++ coursera capstone.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the coursera capstone project
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[ ]:
+
+
+print('Hello Capstone Project Course!')
+
